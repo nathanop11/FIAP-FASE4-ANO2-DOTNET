a022b00e2b762c1e8d4ad1c3af8ae3ed0f5f39d3
Flavio Moreni <PF0954@fiap.com.br>
2024-05-02 23:43:53 -0300

    setup do ef e ambiente

---
+++ Fiap.Web.Alunos/Data/Contexts/DatabaseContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Fiap.Web.Alunos.Data.Contexts
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+        protected DatabaseContext()
+        {
+        }
+    }
+}
